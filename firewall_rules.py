cfab595a2b8374ab29ee6cf8b2e1a82818c8bb31
Maralbyek Tilyek <maralbektilek@gmail.com>
2025-12-02 19:38:18 +0800

    adding additional ports

--- firewall_rules.py
+++ firewall_rules.py
@@ -1,4 +1,3 @@
-# Pre-registered allowed IPs and their allowed ports
 allowed_rules = {
     "192.168.1.10": [22, 80, 443],
     "10.0.0.5": [8080, 3306],
@@ -6,13 +5,11 @@ allowed_rules = {
 }
 
 def check_connection(ip, port):
-    # Check if IP exists
     if ip not in allowed_rules:
         return f"[ALERT] Suspicious IP detected: {ip} is NOT registered!"
-
-    # Check if port is allowed for that IP
+        
     if port not in allowed_rules[ip]:
         return f"[BLOCKED] {ip}:{port} is NOT permitted!"
-
-    # Valid IP and port
+        
     return f"[ALLOWED] {ip}:{port} connection approved."
+
